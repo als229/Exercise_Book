79cab6bcc0ee5aa26e8d1da2e17cb5da5d791dfe
als229 <rhksalsdldia@naver.com>
2021-10-26 00:51:51 +0900

    6-18 까지

--- src/com/javajungsuk/oop/six/Six_Exercise.java
+++ src/com/javajungsuk/oop/six/Six_Exercise.java
@@ -50,15 +50,120 @@ package com.javajungsuk.oop.six;
 //	}
 //
 //}
+
+//6-3
+//public class Six_Exercise {
+//	 public static void main(String[] args) {
+//		Student s = new Student();
+//		s.name = "홍길동";
+//		s.ban = 1 ;
+//		s.no = 1;
+//		s.kor = 100;
+//		s.eng = 60;
+//		s.math = 76;
+//		
+//		System.out.println("이름 : " + s.name);
+//		System.out.println("총점 : " + s.getTotal());
+//		System.out.println("평균 : " + s.getAverage());
+//	}
+//		
+//	 public static class Student{
+//		 
+//		 String name;
+//		 int ban;
+//		 int no;
+//		 int kor;
+//		 int eng;
+//		 int math;
+//		 
+//		 Student(){}
+//		 
+//		 public int getTotal() {
+//			 return kor + eng + math;
+//		 }
+//		 
+//		 public float getAverage(){
+//			 
+//			 return (int)((kor + eng + math)/3f *10 +0.5) /10f;
+//		 }
+//		 
+//	 }
+//	
+//	
+//	
+//
+
+//4번 답
+//public class Six_Exercise {
+//	 static double getDistance(int x, int y, int x1, int y1) {
+//		 return Math.sqrt((x - x1)*(x - x1) + (y - y1)*(y - y1));
+//	 }
+//	public static void main(String[] args) {
+//		System.out.println(getDistance(1, 1, 2, 2));
+//		
+//	}
+//}
+
+
+//6-6
+//class MyPoint{
+//	int x;
+//	int y;
+//
+//	MyPoint(int x , int y){
+//		this.x = x;
+//		this.y = y;
+//	}
+//	public double getDistance(int x1, int y1) {
+//
+//		return Math.sqrt((x - x1)*(x - x1) + (y - y1)*(y - y1));
+//	}
+//}
+//
+//public class Six_Exercise {
+//	public static void main(String[] args) {
+//		MyPoint p = new MyPoint(1, 1);
+//		
+//		System.out.println(p.getDistance(2, 2));
+//
+//	}
+//}
+
+//6-18
+//public class Six_Exercise {
+//	
+//	public static boolean isNumber(String str) {
+//		if (str == null || str == "") {
+//			return false;
+//		}
+//		
+//		for(int i = 0; i <= str.length()-1; i++) {
+//			char ch = str.charAt(i) ;
+//		
+//			if(ch < '0' || '9' < ch) {
+//				return false;
+//			} 
+//		}
+//		return true;
+//	}
+//	
+//	public static void main(String[] args) {
+//		String str = "123";
+//		System.out.println(str + "는 숫자입니까?" + isNumber(str));
+//		
+//		str = "1234o";
+//		System.out.println(str + "는 숫자입니까? " + isNumber(str));
+//		
+//	}
+//}
+
+
+//6-17
 public class Six_Exercise {
-	 public static void main(String[] args) {
+	public static void main(String[] args) {
 		
-		 
+		
 	}
-		
-	
-	
-	
 }
 
 
