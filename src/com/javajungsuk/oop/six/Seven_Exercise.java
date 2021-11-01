9b24453016c732544b174857c9b6a7ca4d70d7e4
als229 <rhksalsdldia@naver.com>
2021-11-01 23:59:14 +0900

    시험문제

---
+++ src/com/javajungsuk/oop/six/Seven_Exercise.java
@@ -0,0 +1,10 @@
+package com.javajungsuk.oop.six;
+
+public class Seven_Exercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/km/exercise01/sihumrun.java
@@ -0,0 +1,11 @@
+package com.km.exercise01;
+
+public class sihumrun {
+
+	public static void main(String[] args) {
+		sihum s = new sihum();
+		s.plus();
+		
+	}
+
+}
