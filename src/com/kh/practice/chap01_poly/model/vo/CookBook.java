f397e02f970df5134342fd50a4f09490056e30c2
als229 <rhksalsdldia@naver.com>
2021-11-06 18:59:44 +0900

    !!!

--- src/com/kh/practice/chap01_poly/model/vo/CookBook.java
+++ src/com/kh/practice/chap01_poly/model/vo/CookBook.java
@@ -21,7 +21,7 @@ public class CookBook extends Book{
 	
 	@Override
 	public String toString() {
-		return "CookBook" + super.toString() + " coupon=" + coupon+ "]";
+		return "CookBook" + super.toString() + " coupon=" + coupon+ "]!!!";
 	}
 	
 }
